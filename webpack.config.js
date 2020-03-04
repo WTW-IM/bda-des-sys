0584af84c26f2463963153fe03b8a78e275413d0
Brian Kerr <brian.kerr@liazon.com>
2020-03-04 13:46:15 -0500

    Updating file and folder structures.

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const PreloadWebpackPlugin = require('preload-webpack-plugin');
-const CssUrlRelativePlugin = require('css-url-relative-plugin');
-const glob = require('glob');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PreloadWebpackPlugin = require("preload-webpack-plugin");
+const CssUrlRelativePlugin = require("css-url-relative-plugin");
+const glob = require("glob");
 
-const IS_DEV = process.env.NODE_ENV === 'dev';
+const IS_DEV = process.env.NODE_ENV === "dev";
 
 const config = {
-  mode: IS_DEV ? 'development' : 'production',
-  devtool: IS_DEV ? 'eval-source-map' : 'source-map',
-  entry: './src/js/index.js',
+  mode: IS_DEV ? "development" : "production",
+  devtool: IS_DEV ? "eval-source-map" : "source-map",
+  entry: "./src/bda-des-sys/assets/js/index.js",
   output: {
-    filename: 'js/[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: IS_DEV ? '' : '/bda-des-sys/'
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: IS_DEV ? "" : "/bda-des-sys/"
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: "babel-loader"
       },
       {
         test: /\.(eot|otf|woff|woff2|ttf)$/,
         use: {
-            loader: 'file-loader',
-            options: {
-                name: '[name].[hash].[ext]'
-            },
-        },
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]"
+          }
+        }
       },
       {
         test: /\.scss$/,
@@ -42,43 +42,43 @@ const config = {
             loader: MiniCssExtractPlugin.loader,
             options: {
               hmr: IS_DEV
-            },
+            }
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               sourceMap: true
             }
           },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
               sourceMap: true
             }
           }
-        ],
+        ]
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 8192,
-              name: '[name].[ext]',
-              fallback: 'file-loader',
-              outputPath: 'images'
-            },
+              name: "[name].[ext]",
+              fallback: "file-loader",
+              outputPath: "images"
+            }
           },
           {
-            loader: 'image-webpack-loader',
+            loader: "image-webpack-loader",
             options: {
               mozjpeg: {
                 progressive: true,
                 quality: 65
               },
               pngquant: {
-                quality: '65-90',
+                quality: "65-90",
                 speed: 4
               },
               gifsicle: {
@@ -86,61 +86,61 @@ const config = {
               },
               webp: {
                 quality: 75
-              },
-            },
-          },
-        ],
-      },
-    ],
+              }
+            }
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new CleanWebpackPlugin(),
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'windows.jQuery': 'jquery'
+      $: "jquery",
+      jQuery: "jquery",
+      "windows.jQuery": "jquery"
     }),
     new CopyWebpackPlugin([
       {
-        from: './src/assets',
-        to: './assets'
-      },
+        from: "./src/assets",
+        to: "./assets"
+      }
     ]),
     new MiniCssExtractPlugin({
-      filename: IS_DEV ? 'css/[name].css' : 'css/[name].[contenthash].css',
-      chunkFilename: 'css/main.css'
+      filename: IS_DEV ? "css/[name].css" : "css/[name].[contenthash].css",
+      chunkFilename: "css/main.css"
     }),
     new webpack.HashedModuleIdsPlugin(),
     new PreloadWebpackPlugin({
-      include: 'initial'
+      include: "initial"
     }),
     new CssUrlRelativePlugin(),
     new webpack.SourceMapDevToolPlugin()
   ],
   devServer: {
     port: 3200,
-    contentBase: path.join(__dirname, 'src')
+    contentBase: path.join(__dirname, "src")
   },
   optimization: {
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /node_modules/,
-          chunks: 'initial',
-          name: 'vendor',
+          chunks: "initial",
+          name: "vendor",
           priority: 10,
           enforce: true
-        },
-      },
+        }
+      }
     },
-    minimizer: [],
-  },
+    minimizer: []
+  }
 };
 
 if (!IS_DEV) {
-  const TerserPlugin = require('terser-webpack-plugin');
-  const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+  const TerserPlugin = require("terser-webpack-plugin");
+  const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
   config.optimization.minimizer.push(
     new TerserPlugin(),
@@ -148,14 +148,14 @@ if (!IS_DEV) {
   );
 }
 
-const files = glob.sync('./src/**/*.html');
+const files = glob.sync("./src/**/*.html");
 
 files.forEach(file => {
   config.plugins.push(
     new HtmlWebPackPlugin({
       filename: path.basename(file),
       template: file,
-      favicon: path.resolve(__dirname, './src/favicon.ico')
+      favicon: path.resolve(__dirname, "./src/favicon.ico")
     })
   );
 });
