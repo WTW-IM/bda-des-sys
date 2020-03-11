411d531e8702d10f0d5f769c44273e8e54025a57
gcolbath <colbath@gmail.com>
2020-03-11 16:34:36 -0600

    Partial fix for the docs' side-nav's ScrollSpy and stickiness.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,11 +2,7 @@ import 'bootstrap';
 
 import "../scss/index.scss";
 
-$("#alert").click(() => {
-  alert("jQuery works!");
-});
-
-// Your jQuery code
+// BDS jQuery code
 
 $(".multi-tier ul a").click(function() {
   var _this = $(this);
