a4fc3bebef502e4452d75b6ab0c95cb992ab4a20
Steve Matney <steve.matney@extendhealth.com>
2023-01-27 15:31:12 -0700

    Fixing linting issues

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,32 +1,32 @@
 import 'bootstrap';
 
-import "../scss/index.scss";
+import '../scss/index.scss';
 
 // BDS jQuery code
 
-$(".multi-tier ul a").click(function() {
+$('.multi-tier ul a').click(function () {
   var _this = $(this);
-  _this.siblings().removeClass("active");
-  _this.addClass("active");
+  _this.siblings().removeClass('active');
+  _this.addClass('active');
 });
 
-$(".multi-tier .folder > a").click(function() {
+$('.multi-tier .folder > a').click(function () {
   var fldr = $(this).parent();
-  if (fldr.hasClass("open")) {
-    fldr.removeClass("open");
+  if (fldr.hasClass('open')) {
+    fldr.removeClass('open');
   } else {
-    fldr.addClass("open");
+    fldr.addClass('open');
   }
   return false;
 });
 
-if (navigator.userAgent.indexOf("Safari") > -1) {
-  $('[data-trigger="focus"]').click(function() {
+if (navigator.userAgent.indexOf('Safari') > -1) {
+  $('[data-trigger="focus"]').click(function () {
     $(this).focus();
   });
-};
+}
 
-$('body').scrollspy({target: '.bs-docs-sidebar'});
+$('body').scrollspy({ target: '.bs-docs-sidebar' });
 
 $('[data-toggle="popover"]').popover();
 
--- src/assets/js/proto/quote-config.js
+++ src/assets/js/proto/quote-config.js
@@ -1,4 +1,4 @@
-$(function(){
+$(() => {
   var _what = $('.step.what');
   var _medicare = $('.step.who.medicare');
   var _ifp = $('.step.who.ifp');
@@ -6,25 +6,25 @@ $(function(){
   var _cta = $('.cta');
   var fadeTime = 250;
   var _selectedProduct;
-  
+
   // == Check for selected HHMs for hide/show of date, ZIP, and CTA ============
-  var checkSelected = function(){
+  var checkSelected = function () {
     if ($('.step.who:visible .btn-icon').hasClass('selected')) {
-      _whenWhere.fadeIn(fadeTime, function(){
+      _whenWhere.fadeIn(fadeTime, () => {
         _cta.fadeIn(fadeTime);
       });
     } else {
-      _cta.fadeOut(fadeTime, function(){
+      _cta.fadeOut(fadeTime, () => {
         _whenWhere.fadeOut(fadeTime);
       });
     }
   };
-  
+
   // == People selection =======================================================
-  $('.option-selector .btn-icon').not(':disabled, .disabled').click(function(){
+  $('.option-selector .btn-icon').not(':disabled, .disabled').click(function () {
     var _this = $(this);
     var multiSel = _this.parent().parent().hasClass('multi-select');
-    
+
     if (multiSel) {
       if (_this.hasClass('selected')) {
         _this.removeClass('selected');
@@ -36,87 +36,85 @@ $(function(){
       sibs.removeClass('selected');
       _this.addClass('selected');
     }
-    
+
     checkSelected();
   });
-  
+
   // == Switch between product types (Medicare, IFP, etc.) =====================
-  $('.step.what button').click(function(){
+  $('.step.what button').click(function () {
     var _this = $(this);
-    
+
     if (_this.text().toLowerCase().search('medicare') >= 0) {
       if (_ifp.is(':visible')) {
-        _ifp.fadeOut(fadeTime, function(){
-          _medicare.fadeIn(fadeTime, function(){
+        _ifp.fadeOut(fadeTime, () => {
+          _medicare.fadeIn(fadeTime, () => {
             checkSelected();
           });
         });
       } else {
-        _medicare.fadeIn(fadeTime, function(){
+        _medicare.fadeIn(fadeTime, () => {
           checkSelected();
         });
       }
     } else if (_this.text().toLowerCase().search('family') >= 0) {
       if (_medicare.is(':visible')) {
-        _medicare.fadeOut(fadeTime, function(){
-          _ifp.fadeIn(fadeTime, function(){
+        _medicare.fadeOut(fadeTime, () => {
+          _ifp.fadeIn(fadeTime, () => {
             checkSelected();
           });
         });
       } else {
-        _ifp.fadeIn(fadeTime, function(){
+        _ifp.fadeIn(fadeTime, () => {
           checkSelected();
         });
       }
     }
-    
+
     _selectedProduct = _what.find('.selected').attr('id').split('-')[0];
   });
-  
+
   // == ESRD messaging =========================================================
-  _medicare.find('[type=checkbox]').change(function(){
+  _medicare.find('[type=checkbox]').change(function () {
     var _this = $(this);
     var _checked = this.checked;
     var _button = _this.parents('.selection-row').find('button.btn-icon');
     var _otherBtns = _this.parents('.option-selector').find('button.btn-icon');
     var _alert = _this.parents('.selection-row').find('.alert');
-    
+
     if (_checked) {
-      _button.removeClass('selected', function(){
+      _button.removeClass('selected', () => {
         _button.prop('disabled', true);
       });
-      _alert.fadeIn(fadeTime, function() {
-        _cta.fadeOut(fadeTime, function(){
+      _alert.fadeIn(fadeTime, () => {
+        _cta.fadeOut(fadeTime, () => {
           _whenWhere.fadeOut(fadeTime);
         });
       });
     } else {
-      _button.prop('disabled', function(){
-        if(_otherBtns.hasClass('selected')) {
+      _button.prop('disabled', () => {
+        if (_otherBtns.hasClass('selected')) {
           _alert.fadeOut(fadeTime);
           return false;
-        } else {
-          _alert.fadeOut(fadeTime, function(){
-            _whenWhere.fadeIn(fadeTime, function(){
-              _cta.fadeIn(fadeTime);
-            });
-          });
-          _button.addClass('selected');
-          return false;
         }
+        _alert.fadeOut(fadeTime, () => {
+          _whenWhere.fadeIn(fadeTime, () => {
+            _cta.fadeIn(fadeTime);
+          });
+        });
+        _button.addClass('selected');
+        return false;
       });
     }
   });
-  
-  
+
   // == Popovers for inelegibles ===============================================
-  $('.step.who .btn-icon[disabled]').click(function(){
+  $('.step.who .btn-icon[disabled]').click(function () {
     console.log('Disabled person button clicked...');
-    
+
     var _this = $(this);
-    
-    switch(_selectedProduct) {
-      case 'medicare':
+
+    switch (_selectedProduct) {
+      case 'medicare': {
         _this.popover({
           content: '<p>This family member is eligible for an Individual & Family plan.</p><p>To shop for this family member, select "Individual & Family" in the previous step.</p>',
           html: true,
@@ -126,7 +124,8 @@ $(function(){
           placement: 'right'
         });
         break;
-      case 'ifp':
+      }
+      case 'ifp': {
         _this.popover({
           content: '<p>This family member is eligible for Medicare coverage.</p><p>To shop for this family member, select "Medicare" in the previous step.</p>',
           html: true,
@@ -136,16 +135,17 @@ $(function(){
           placement: 'right'
         });
         break;
-      default:
+      }
+      default: {
         return false;
+      }
     }
   });
-  
-  
+
   // == Display realistic effective date =======================================
   var now = new Date();
-  var monthArray = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-  
+  var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
   if (now.getMonth() == 11 && now.getDate() >= 15) {
     var current = new Date(now.getFullYear() + 1, 1, 1);
   } else if (now.getMonth() == 11) {
@@ -155,44 +155,43 @@ $(function(){
   } else {
     var current = new Date(now.getFullYear(), now.getMonth() + 1, 1);
   }
-  
+
   var monthToShow = monthArray[current.getMonth()];
-  
+
   $('#effectiveDate').text(monthToShow);
-  
-  
+
   // == Proceed to Plans Logic =================================================
-  $('#view-plans-btn').click(function(){
+  $('#view-plans-btn').click(() => {
     var _urlPrefix = '/qc-uat-q3-2016-quoting';
-    
+
     switch (_selectedProduct) {
-      case 'medicare':
+      case 'medicare': {
         window.location = _urlPrefix + '-medicare.html';
         break;
-      case 'ifp':
-        window.location = _urlPrefix + '.html'
+      }
+      case 'ifp': {
+        window.location = _urlPrefix + '.html';
         break;
-      default:
+      }
+      default: {
         return false;
+      }
     }
   });
-  
-  
+
   // == Engage Popovers! =======================================================
-  $("[data-toggle=popover]").popover();
-  
-  
+  $('[data-toggle=popover]').popover();
+
   // == Force Popovers to work in FF, Safari, etc... o_O =======================
   // Firefox 1.0+
   var isFirefox = typeof InstallTrigger !== 'undefined';
-  
-  // Safari 3.0+ "[object HTMLElementConstructor]" 
-  var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0 || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
+
+  // Safari 3.0+ "[object HTMLElementConstructor]"
+  var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0 || (function (p) { return p.toString() === '[object SafariRemoteNotification]'; }(!window.safari || safari.pushNotification));
 
   if (isFirefox || isSafari) {
-    $('[data-toggle="popover"]').click(function(){
+    $('[data-toggle="popover"]').click(function () {
       $(this).focus();
     });
   }
-  
 });
